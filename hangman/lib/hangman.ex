cb5fd6cf5646a52e9985633201fd47f3a87d3ac0
Vinicius <130008311+0bvim@users.noreply.github.com>
2025-10-22 01:05:12 -0300

    feat: start game implementation
    - Add dependencies to mix.exs
    - Add types and specs

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,9 +1,20 @@
 defmodule Hangman do
+ 
+  alias Hangman.Impl.Game
+  @type state :: :initializng | :won | :lost | :goog_guess | :bad_guess | :already_used
+  @opaque game :: Game.t
+  @type tally :: %{
+    turns_left: integer,
+    game_state: state,
+    letters: list(String.t),
+    used: list(String.t)
+  }
 
-  def new_game do
-  end
+  @spec new_game() :: game
+  defdelegate new_game, to: Game, as: :init_game
 
-  def make_move(game, guess) do
+  @spec make_move(game, String.t) :: { game, tally }
+  def make_move(_game, _guess) do
   end
 
 end
