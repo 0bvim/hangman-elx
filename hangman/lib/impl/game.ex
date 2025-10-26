dc8445a61cb8e4dbf72d8811042ea5d24965bbe5
Vinicius <130008311+0bvim@users.noreply.github.com>
2025-10-26 15:04:55 -0300

    feat: add check of duplicated moves
    - Added tests for those created functions too

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -16,6 +16,8 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
   
+###############################################################################
+  
   @spec new_game() :: t
   def new_game do
     new_game(Dictionary.random_word)
@@ -30,13 +32,32 @@ defmodule Hangman.Impl.Game do
       |> String.codepoints
     }
   end
+ 
+###############################################################################
   
   @spec make_move(t, String.t) :: { t, Type.tally }
   def make_move(game = %{ game_state: state }, _guess) 
   when state in [:won, :lost] do
-    { game, tally(game) }
+    return_with_tally(game)
   end
 
+  def make_move(game, guess) do
+    accept_guess(game, guess, MapSet.member?(game.used, guess))
+    |> return_with_tally()
+  end
+
+###############################################################################
+  
+  defp accept_guess(game, _guess, _already_used = true) do
+    %{ game | game_state: :already_used }
+  end
+  
+  defp accept_guess(game, guess, _already_used) do
+    %{ game | used: MapSet.put(game.used, guess) }
+  end
+
+###############################################################################
+  
   defp tally(game) do
     %{
       turns_left: game.turns_left,
@@ -45,4 +66,11 @@ defmodule Hangman.Impl.Game do
       used: game.used |> MapSet.to_list |> Enum.sort
     }
   end
+
+###############################################################################
+  
+  defp return_with_tally(game) do
+    { game, tally(game) }
+  end
+  
 end
