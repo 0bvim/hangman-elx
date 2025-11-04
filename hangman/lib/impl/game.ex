d142e085a7ff8199996e2c0ea075f048f6400ae8
Vinicius <130008311+0bvim@users.noreply.github.com>
2025-11-04 00:47:42 -0300

    feat: finish text client.
    - Improve return revealed word when lost game. - Adjust game tests

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,13 +1,12 @@
 defmodule Hangman.Impl.Game do
-
   alias Hangman.Type
 
   @type t :: %__MODULE__{
-    turns_left: integer,
-    game_state: Type.state,
-    letters: list(String.t),
-    used: MapSet.t(String.t)
-  }
+          turns_left: integer,
+          game_state: Type.state(),
+          letters: list(String.t()),
+          used: MapSet.t(String.t())
+        }
 
   defstruct(
     turns_left: 7,
@@ -16,28 +15,29 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
-###############################################################################
+  ###############################################################################
 
   @spec new_game() :: t
   def new_game do
-    new_game(Dictionary.random_word)
+    new_game(Dictionary.random_word())
   end
 
-  @spec new_game(String.t) :: t
+  @spec new_game(String.t()) :: t
   def new_game(word) do
     %__MODULE__{
-      letters: word
-      |> String.trim
-      |> String.downcase
-      |> String.codepoints
+      letters:
+        word
+        |> String.trim()
+        |> String.downcase()
+        |> String.codepoints()
     }
   end
 
-###############################################################################
+  ###############################################################################
 
-  @spec make_move(t, String.t) :: { t, Type.tally }
-  def make_move(game = %{ game_state: state }, _guess)
-  when state in [:won, :lost] do
+  @spec make_move(t, String.t()) :: {t, Type.tally()}
+  def make_move(game = %{game_state: state}, _guess)
+      when state in [:won, :lost] do
     return_with_tally(game)
   end
 
@@ -46,47 +46,50 @@ defmodule Hangman.Impl.Game do
     |> return_with_tally()
   end
 
-###############################################################################
+  ###############################################################################
 
   defp accept_guess(game, _guess, _already_used = true) do
-    %{ game | game_state: :already_used }
+    %{game | game_state: :already_used}
   end
 
   defp accept_guess(game, guess, _already_used) do
-    %{ game | used: MapSet.put(game.used, guess) }
+    %{game | used: MapSet.put(game.used, guess)}
     |> score_guess(Enum.member?(game.letters, guess))
   end
 
-###############################################################################
+  ###############################################################################
 
   defp score_guess(game, _goog_guess = true) do
     # guessed all letters? -> :won | :good_guess
     new_state = maybe_won(MapSet.subset?(MapSet.new(game.letters), game.used))
-    %{ game | game_state: new_state }
+    %{game | game_state: new_state}
   end
 
-  defp score_guess(game = %{ turns_left: 1 }, _bad_guess) do
-    %{ game | game_state: :lost, turns_left: 0 }
+  defp score_guess(game = %{turns_left: 1}, _bad_guess) do
+    %{game | game_state: :lost, turns_left: 0}
   end
 
   defp score_guess(game, _bad_guess) do
-    %{ game | game_state: :bad_guess, turns_left: game.turns_left - 1 }
-    # turns_left == 1 -> lost | dec turns_left, :bad_guess
+    %{game | game_state: :bad_guess, turns_left: game.turns_left - 1}
   end
 
-###############################################################################
+  ###############################################################################
 
   def tally(game) do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
       letters: reveal_guessed_letters(game),
-      used: game.used |> MapSet.to_list |> Enum.sort
+      used: game.used |> MapSet.to_list() |> Enum.sort()
     }
   end
 
   defp return_with_tally(game) do
-    { game, tally(game) }
+    {game, tally(game)}
+  end
+
+  defp reveal_guessed_letters(game = %{game_state: :lost}) do
+    game.letters
   end
 
   defp reveal_guessed_letters(game) do
--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -3,7 +3,7 @@ defmodule HangmanImplGameTest do
   alias Hangman.Impl.Game
 
   test "new game returns structure" do
-    game = Game.new_game
+    game = Game.new_game()
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
@@ -20,61 +20,61 @@ defmodule HangmanImplGameTest do
     for state <- [:won, :lost] do
       game = Game.new_game("bat")
       game = Map.put(game, :game_state, state)
-      { new_game, _tally } = Game.make_move(game, "x")
+      {new_game, _tally} = Game.make_move(game, "x")
       assert new_game == game
     end
   end
 
   test "a duplicated letter is reported" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "x")
     assert game.game_state != :already_used
-    { game, _tally } = Game.make_move(game, "y")
+    {game, _tally} = Game.make_move(game, "y")
     assert game.game_state != :already_used
-    { game, _tally } = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "we record letters used" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
-    { game, _tally } = Game.make_move(game, "y")
-    { game, _tally } = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "y")
+    {game, _tally} = Game.make_move(game, "x")
     assert MapSet.equal?(game.used, MapSet.new(["x", "y"]))
   end
 
   test "we recognize a letter in the word" do
     game = Game.new_game("wombat")
-    { _game, tally } = Game.make_move(game, "m")
+    {_game, tally} = Game.make_move(game, "m")
     assert tally.game_state == :good_guess
-    { _game, tally } = Game.make_move(game, "t")
+    {_game, tally} = Game.make_move(game, "t")
     assert tally.game_state == :good_guess
   end
 
   test "we recognize a letter not in ther word" do
     game = Game.new_game("wombat")
-    { game, tally } = Game.make_move(game, "n")
+    {game, tally} = Game.make_move(game, "n")
     assert tally.game_state == :bad_guess
-    { _game, tally } = Game.make_move(game, "e")
+    {_game, tally} = Game.make_move(game, "e")
     assert tally.game_state == :bad_guess
   end
 
   test "decrease turns when wrong guess" do
     game = Game.new_game("wombat")
-    { game, tally } = Game.make_move(game, "n")
+    {game, tally} = Game.make_move(game, "n")
     assert tally.turns_left == 6
-    { _game, tally } = Game.make_move(game, "e")
+    {_game, tally} = Game.make_move(game, "e")
     assert tally.turns_left == 5
   end
 
-# hello
+  # hello
   test "can handle a sequence of moves" do
     [
       # guess | state | turns | letters | used
-      ["a", :bad_guess, 6, [ "_", "_", "_","_","_" ], [ "a" ] ],
-      ["a", :already_used, 6, [ "_", "_", "_","_","_" ], [ "a" ] ],
-      ["e", :good_guess, 6, [ "_", "e", "_","_","_" ], [ "a", "e" ] ],
-      ["x", :bad_guess, 5, [ "_", "e", "_","_","_" ], [ "a", "e", "x" ] ],
+      ["a", :bad_guess, 6, ["_", "_", "_", "_", "_"], ["a"]],
+      ["a", :already_used, 6, ["_", "_", "_", "_", "_"], ["a"]],
+      ["e", :good_guess, 6, ["_", "e", "_", "_", "_"], ["a", "e"]],
+      ["x", :bad_guess, 5, ["_", "e", "_", "_", "_"], ["a", "e", "x"]]
     ]
     |> test_sequence_of_moves()
   end
@@ -82,19 +82,49 @@ defmodule HangmanImplGameTest do
   test "can handle a winning game" do
     [
       # guess | state | turns | letters | used
-      ["a", :bad_guess,       6, [ "_", "_", "_","_","_" ], Enum.sort([ "a" ]) ],
-      ["a", :already_used,    6, [ "_", "_", "_","_","_" ], Enum.sort([ "a" ]) ],
-      ["e", :good_guess,      6, [ "_", "e", "_","_","_" ], Enum.sort([ "a", "e" ]) ],
-      ["x", :bad_guess,       5, [ "_", "e", "_","_","_" ], Enum.sort([ "a", "e", "x" ]) ],
-      ["x", :already_used,    5, [ "_", "e", "_","_","_" ], Enum.sort([ "a", "e", "x" ]) ],
-      ["o", :good_guess,      5, [ "_", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o" ]) ],
-      ["i", :bad_guess,       4, [ "_", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i" ]) ],
-      ["j", :bad_guess,       3, [ "_", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j" ]) ],
-      ["h", :good_guess,      3, [ "h", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h" ]) ],
-      ["h", :already_used,    3, [ "h", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h" ]) ],
-      ["k", :bad_guess,       2, [ "h", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h", "k" ]) ],
-      ["v", :bad_guess,       1, [ "h", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h", "k", "v" ]) ],
-      ["l", :won,             1, [ "h", "e", "l","l","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h", "k", "v", "l" ]) ],
+      ["a", :bad_guess, 6, ["_", "_", "_", "_", "_"], Enum.sort(["a"])],
+      ["a", :already_used, 6, ["_", "_", "_", "_", "_"], Enum.sort(["a"])],
+      ["e", :good_guess, 6, ["_", "e", "_", "_", "_"], Enum.sort(["a", "e"])],
+      ["x", :bad_guess, 5, ["_", "e", "_", "_", "_"], Enum.sort(["a", "e", "x"])],
+      ["x", :already_used, 5, ["_", "e", "_", "_", "_"], Enum.sort(["a", "e", "x"])],
+      ["o", :good_guess, 5, ["_", "e", "_", "_", "o"], Enum.sort(["a", "e", "x", "o"])],
+      ["i", :bad_guess, 4, ["_", "e", "_", "_", "o"], Enum.sort(["a", "e", "x", "o", "i"])],
+      ["j", :bad_guess, 3, ["_", "e", "_", "_", "o"], Enum.sort(["a", "e", "x", "o", "i", "j"])],
+      [
+        "h",
+        :good_guess,
+        3,
+        ["h", "e", "_", "_", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h"])
+      ],
+      [
+        "h",
+        :already_used,
+        3,
+        ["h", "e", "_", "_", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h"])
+      ],
+      [
+        "k",
+        :bad_guess,
+        2,
+        ["h", "e", "_", "_", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h", "k"])
+      ],
+      [
+        "v",
+        :bad_guess,
+        1,
+        ["h", "e", "_", "_", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h", "k", "v"])
+      ],
+      [
+        "l",
+        :won,
+        1,
+        ["h", "e", "l", "l", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h", "k", "v", "l"])
+      ]
     ]
     |> test_sequence_of_moves()
   end
@@ -102,19 +132,49 @@ defmodule HangmanImplGameTest do
   test "can handle a losing game" do
     [
       # guess | state | turns | letters | used
-      ["a", :bad_guess,       6, [ "_", "_", "_","_","_" ], Enum.sort([ "a" ]) ],
-      ["a", :already_used,    6, [ "_", "_", "_","_","_" ], Enum.sort([ "a" ]) ],
-      ["e", :good_guess,      6, [ "_", "e", "_","_","_" ], Enum.sort([ "a", "e" ]) ],
-      ["x", :bad_guess,       5, [ "_", "e", "_","_","_" ], Enum.sort([ "a", "e", "x" ]) ],
-      ["x", :already_used,    5, [ "_", "e", "_","_","_" ], Enum.sort([ "a", "e", "x" ]) ],
-      ["o", :good_guess,      5, [ "_", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o" ]) ],
-      ["i", :bad_guess,       4, [ "_", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i" ]) ],
-      ["j", :bad_guess,       3, [ "_", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j" ]) ],
-      ["h", :good_guess,      3, [ "h", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h" ]) ],
-      ["h", :already_used,    3, [ "h", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h" ]) ],
-      ["k", :bad_guess,       2, [ "h", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h", "k" ]) ],
-      ["v", :bad_guess,       1, [ "h", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h", "k", "v" ]) ],
-      ["m", :lost,            0, [ "h", "e", "_","_","o" ], Enum.sort([ "a", "e", "x", "o", "i", "j", "h", "k", "v", "m" ]) ],
+      ["a", :bad_guess, 6, ["_", "_", "_", "_", "_"], Enum.sort(["a"])],
+      ["a", :already_used, 6, ["_", "_", "_", "_", "_"], Enum.sort(["a"])],
+      ["e", :good_guess, 6, ["_", "e", "_", "_", "_"], Enum.sort(["a", "e"])],
+      ["x", :bad_guess, 5, ["_", "e", "_", "_", "_"], Enum.sort(["a", "e", "x"])],
+      ["x", :already_used, 5, ["_", "e", "_", "_", "_"], Enum.sort(["a", "e", "x"])],
+      ["o", :good_guess, 5, ["_", "e", "_", "_", "o"], Enum.sort(["a", "e", "x", "o"])],
+      ["i", :bad_guess, 4, ["_", "e", "_", "_", "o"], Enum.sort(["a", "e", "x", "o", "i"])],
+      ["j", :bad_guess, 3, ["_", "e", "_", "_", "o"], Enum.sort(["a", "e", "x", "o", "i", "j"])],
+      [
+        "h",
+        :good_guess,
+        3,
+        ["h", "e", "_", "_", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h"])
+      ],
+      [
+        "h",
+        :already_used,
+        3,
+        ["h", "e", "_", "_", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h"])
+      ],
+      [
+        "k",
+        :bad_guess,
+        2,
+        ["h", "e", "_", "_", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h", "k"])
+      ],
+      [
+        "v",
+        :bad_guess,
+        1,
+        ["h", "e", "_", "_", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h", "k", "v"])
+      ],
+      [
+        "m",
+        :lost,
+        0,
+        ["h", "e", "l", "l", "o"],
+        Enum.sort(["a", "e", "x", "o", "i", "j", "h", "k", "v", "m"])
+      ]
     ]
     |> test_sequence_of_moves()
   end
@@ -124,8 +184,8 @@ defmodule HangmanImplGameTest do
     Enum.reduce(script, game, &check_one_move/2)
   end
 
-  defp check_one_move([ guess, state, turns, letters, used], game) do
-    { game, tally } = Game.make_move(game, guess)
+  defp check_one_move([guess, state, turns, letters, used], game) do
+    {game, tally} = Game.make_move(game, guess)
 
     assert tally.game_state == state
     assert tally.turns_left == turns
