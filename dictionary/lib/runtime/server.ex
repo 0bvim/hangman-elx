dbc6ae5cdeb2f0aa232cd15e48043aa47e9c68b5
vinicius <niviciusdev@gmail.com>
2026-04-21 01:23:29 -0300

    Implement Hangman runtime server using GenServer

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -2,9 +2,9 @@ defmodule Dictionary.Runtime.Server do
   @type t :: pid()
 
   @me __MODULE__
-  
+
   use Agent
-  
+
   alias Dictionary.Impl.WordList
 
   def start_link(_) do
@@ -12,9 +12,6 @@ defmodule Dictionary.Runtime.Server do
   end
 
   def random_word() do
-    if :rand.uniform < 0.33 do
-      Agent.get(@me, fn _ -> exit(:boom) end)
-    end
     Agent.get(@me, &WordList.random_word/1)
   end
 end
