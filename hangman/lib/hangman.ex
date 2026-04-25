b8d70bf35dc23edf3c36da9f283e50fbb032eaf5
vinicius <niviciusdev@gmail.com>
2026-04-25 20:13:58 -0300

    Switch Hangman API to Runtime Server GenServer
    Replace delegations to Hangman.Impl.Game with a runtime
    GenServer (Hangman.Runtime.Server). new_game starts the server and
    returns
    its pid; make_move and tally use GenServer.call. Add Server.t as pid
    type.

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,15 +1,22 @@
 defmodule Hangman do
-  alias Hangman.Impl.Game
+  alias Hangman.Runtime.Server
 
-  @opaque game :: Game.t()
+  @opaque game :: Server.t
   @type tally :: Hangman.tally()
 
-  @spec new_game() :: Hangman.Impl.Game.t()
-  defdelegate new_game, to: Game
+  @spec new_game() :: game
+  def new_game do
+    { :ok, pid } = Server.start_link
+    pid
+  end
 
   @spec make_move(game, String.t()) :: {game, tally}
-  defdelegate make_move(game, guess), to: Game
+  def make_move(game, guess) do
+    GenServer.call(game, {:make_move, guess})    
+  end
 
   @spec tally(game) :: tally()
-  defdelegate tally(game), to: Game
+  def tally(game) do
+    GenServer.call(game, {:tally})
+  end
 end
